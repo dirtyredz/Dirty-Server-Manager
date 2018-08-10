0aeee2dc528cc07d3e0c52389fee15ad9c645d98
dirtyredz <dirtyredz@live.com>
2018-08-10 02:19:51 -0500

    cleanup and process pid for commands

--- src/bin/webServer.js
+++ src/bin/webServer.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const app = express()
+import localStorage from '../lib/localStorage'
+
+localStorage.setItem('WebServerPid',process.pid)
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+
+process.on('beforeExit',()=>{
+  localStorage.clear()
+  process.exit(0)
+})
