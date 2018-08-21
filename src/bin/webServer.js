ed4bdb49d4f12f3acb46b162c2f646c027e8b053
dirtyredz <dirtyredz@live.com>
2018-08-21 00:08:09 -0500

    Website and Server

--- src/bin/webServer.js
+++ src/bin/webServer.js
@@ -1,48 +1,24 @@
-// import express from 'express';
-// import React from 'react';
-// import { renderToString } from 'react-dom/server';
-// import App from '../website/App';
-// import localStorage from '../lib/localStorage'
-// import path from 'path'
-// import * as globals from '../lib/globals'
+import express from 'express';
+import localStorage from '../lib/localStorage'
+import path from 'path'
+import * as globals from '../lib/globals'
 
-// const port = 3000;
-// const server = express();
-// localStorage.setItem('WebServerPid',process.pid)
+const server = express();
+localStorage.setItem('WebServerPid',process.pid)
 
-// // server.use(express.static(path.join(__dirname, 'build')));
-// server.use( '/public', express.static( path.resolve(globals.InstallationDir() + '/dsm/public') ) );
+// server.use(express.static(path.join(__dirname, 'build')));
+server.use( '/public', express.static( path.resolve(globals.InstallationDir() + '/dsm/public') ) );
+server.get('/*', function(req, res){
+  res.sendFile(path.resolve(globals.InstallationDir() + '/dsm/public/index.html'));
+});
 
-// server.get( "/", ( req, res ) => {
-//   const jsx = ( <App /> );
-//   const reactDom = renderToString( jsx );
+server.listen(3000, () => console.log('Example app listening on port 3000!'))
 
-//   res.writeHead( 200, { "Content-Type": "text/html" } );
-//   res.end( htmlTemplate( reactDom ) );
-// } );
+process.on('beforeExit',()=>{
+  localStorage.clear()
+  process.exit(0)
+})
 
-// server.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-// process.on('beforeExit',()=>{
-//   localStorage.clear()
-//   process.exit(0)
-// })
-
-// function htmlTemplate( reactDom ) {
-//   return `
-//       <!DOCTYPE html>
-//       <html>
-//       <head>
-//           <meta charset="utf-8">
-//           <title>ReR</title>
-//       </head>
-//       <body>
-//           <div id="app">${ reactDom }</div>
-//           <script src="./public/webClient.js"></script>
-//       </body>
-//       </html>
-//   `;
-// }
 
 
 
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,128 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require("path");
+const WrapperPlugin = require('wrapper-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const WebpackShellPlugin = require('webpack-shell-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
- dev = {
+const node = {
   watch: true,
   mode: 'development',
+  target: 'node',
+  entry: {
+    dsm: "./src/bin/dsm.js",
+    serverWrapper: './src/bin/serverWrapper.js',
+    webServer: './src/bin/webServer.js',
+  },
+  output: {
+    path: path.resolve(__dirname, "dsm"),
+    filename: "[name].js",
+  },
   plugins: [
-    new WebpackShellPlugin({onBuildExit:['npm install -g ./']})
-  ]
+    new CleanWebpackPlugin(['dsm/dsm.lua','dsm/config.ini'],{watch: true}),
+    new CopyWebpackPlugin([
+      './src/dsm.lua',
+      './src/config.ini'
+    ]),
+    new WebpackShellPlugin({onBuildExit:['npm install -g ./','dsm stop-web && dsm start-web']}),
+    // strict mode for the whole bundle
+    new WrapperPlugin({
+      test: /\.js$/, // only wrap output of bundle files with '.js' extension 
+      header: function (fileName) {
+        return '#!/usr/bin/env node\n';
+      }
+    })
+  ],
+  externals: (ctx, req, done) => (/^node-pty$/.test(req) ? done(null, `commonjs ${req}`) : done()),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "react",
+              [
+                "babel-preset-env",
+                {
+                  "targets": {
+                    "node": "current"
+                  }
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {
+            comments: "/#!\/usr\/bin\/env node/"
+          },
+        }
+      })
+    ]
+  },
+}
+const react = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "react",
+              "env",
+              "stage-0"
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  entry: {
+      webClient: './src/website/index.js'
+  },
+  plugins:[
+    new CleanWebpackPlugin(['dsm/public/*']),
+    new HtmlWebpackPlugin({
+      template: './src/website/public/index.html'
+    })
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dsm/public'),
+    publicPath: '/public/'
+  }
 };
-
-module.exports = merge(common,dev)
+module.exports = [node,react]
