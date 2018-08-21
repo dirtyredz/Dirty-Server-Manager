fbf4f5bf1e741c9b588ea6b2d01b830a27a2798f
dirtyredz <dirtyredz@live.com>
2018-08-20 20:59:56 -0500

    website

--- src/bin/public/webClient.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import { render } from 'react-dom';
-import App from '../website/App';
-
-// render(<App />, document.getElementById('app'));
-
-const app = document.getElementById( "app" );
-ReactDOM.hydrate( <App />, app );
