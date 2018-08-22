5e952380b9822d847636410441cab59b886ec197
dirtyredz <dirtyredz@live.com>
2018-08-22 15:33:35 -0500

    to allow maps to take full content area

--- src/website/components/ContentWrapper/ContentWrapper.js
+++ src/website/components/ContentWrapper/ContentWrapper.js
@@ -4,7 +4,11 @@ import { Header } from '../'
 
 export default class ContentWrapper extends Component {
   render() {
-    const fillStyle = this.props.fill ? {padding: 0, minHeight: "calc(100vh - 75px)"}: {padding: 10, minHeight: "calc(100vh - 95px)"}
+    let fillStyle = this.props.fill ? {padding: 0, minHeight: "calc(100vh - 75px)"}: {padding: 10, minHeight: "calc(100vh - 95px)"}
+    if(this.props.exact){
+      fillStyle.maxHeight = fillStyle.minHeight
+      fillStyle.maxWidth = "calc(100vw - 200px)"
+    }
     return (
       <div className="ContentWrapper">
         <Header height={75}/>
