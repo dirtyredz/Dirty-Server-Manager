32d67df91e6d2f5ca0c77503cb4dd83824ff7cf8
dirtyredz <dirtyredz@live.com>
2018-08-25 23:16:28 -0500

    Added RecursiveOptions to commander

--- src/bin/dsm.js
+++ src/bin/dsm.js
@@ -1,30 +1,43 @@
-import program from 'commander'
+import {Command} from 'Commander'
 import colors from 'colors'
 import * as commands from '../commands/'
 
 // Remove for production
 process.on('warning', e => console.warn(e.stack));
 
+// Add recursive options to Commander
+Command.prototype.recursiveOptions = function(options){
+  if(options.length > 0){
+    options.map(opt=>{
+      return this.option(opt.flag, opt.description)
+    })
+    // return this.option(cmd.option[0], cmd.option[1])
+  }
+  return this
+}
+
+let Commander = new Command()
 /*******************************************/
 // process all commands
 const Commands = Object.keys(commands)
 Commands.map((cmd, index) => {
   cmd = commands[cmd];
   if(cmd.command && cmd.description && typeof cmd.action == 'function'){
-    program
-      .command(cmd.command)
-      .version(cmd.version ? cmd.version : colors.red("No version available"), '-v, --version')
-      .alias(cmd.alias ? cmd.alias : "")
-      .description(cmd.description)
-      .action(cmd.action)
+    Commander
+        .command(cmd.command)
+        .version(cmd.version ? cmd.version : colors.red("No version available"), '-v, --version')
+        .alias(cmd.alias ? cmd.alias : "")
+        .recursiveOptions(cmd.options || [])
+        .description(cmd.description)
+        .action(cmd.action)
   }else{
     console.error('%s',colors.red('Unable to process command: ' + Commands[index]))
   }
 });
 
 // error on unknown commands
-program.on('command:*', function () {
-  console.error(colors.red('Invalid command: %s')+' \nSee '+colors.yellow('--help')+' for a list of available commands.', program.args.join(' '));
+Commander.on('command:*', function () {
+  console.error(colors.red('Invalid command: %s')+' \nSee '+colors.yellow('--help')+' for a list of available commands.', Commander.args.join(' '));
   process.exit(1);
 });
 
@@ -34,9 +47,8 @@ if (typeof process.argv[2] === 'undefined') {
   process.exit(1);
 }
 
-program
+Commander
   .version('0.1.0')
   .usage('[options] <cmd ...>')
   .option('-f, --foo', 'enable some foo')
   .parse(process.argv);
-
