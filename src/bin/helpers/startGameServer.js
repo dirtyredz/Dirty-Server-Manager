70dcbfc471b8f52e9cd2cbe3e8ddefe2012b8168
dirtyredz <dirtyredz@live.com>
2018-09-01 23:01:50 -0500

    moved large comment

--- src/bin/helpers/startGameServer.js
+++ src/bin/helpers/startGameServer.js
@@ -3,79 +3,8 @@ import path from 'path'
 
 // npm install --global --production windows-build-tools
 var pty = require('node-pty');// dont use import, webpack is set to not touch node-pty as it will mess with it.
-
 // https://github.com/Microsoft/node-pty/issues/78
 
-// Allowed options:
-//   --help                         show help message
-//   --version                      print out version and exit
-//   --port arg                     listening port of the server
-//   --query-port arg               internal game query port of the server,
-//                                  default: 27003
-//   --steam-query-port arg         steam query port of the server, default: 27020
-//   --steam-master-port arg        steam master server port of the server,
-//                                  default: 27021
-//   --ip arg                       binds the server to a specific IP (steam
-//                                  networking only)
-//   --max-players arg              maximum number of online players
-//   --save-interval arg            timestep between savings
-//   --server-name arg              server name, will be displayed when queried
-//   --pausable arg                 whether or not the server can be paused by
-//                                  admins when there's only a single player
-//                                  online
-//   --galaxy-name arg              galaxy name, appended to datapath, final path
-//                                  will be [datapath]/[galaxyname]
-//   --datapath arg                 folder the galaxies will be stored in, will be
-//                                  prepended to galaxy name
-//   --admin arg                    steam id(s) of the administrator(s) to add to
-//                                  the server
-//   --seed arg                     seed of the server
-//   --difficulty arg               difficulty of the server, allowed values are:
-//                                  -3, -2, -1, 0, 1, 2, 3
-//   --infinite-resources arg       enable infinite resources for all players
-//   --collision-damage arg         amount of damage done to an object on
-//                                  collision, from 0 to 1. 0: no damage, 1: full
-//                                  damage. default: 1
-//   --same-start-sector arg        indicate if all players should start in the
-//                                  same sector
-//   --alive-sectors-per-player arg the amount of sectors with player property
-//                                  that are simulated in addition to that
-//                                  player's current sector
-//   --safe-player-input arg        enable to guarantee more cheat-safety, but
-//                                  players may experience more lag
-//   --threads arg                  specifies the number of threads used to update
-//                                  the sectors
-//   --generator-threads arg        specifies the number of threads used to
-//                                  generate sectors
-//   -t [ --trace ] arg             tracing options. Can be more than one. Allowed
-//                                  values are: network scripting threading io
-//                                  database input error warning exception user
-//                                  game system debug sound gl all
-//   --exit-on-last-admin-logout    shut down when last administrator logs out
-//   --stderr-to-log                redirect std error output from console to log
-//                                  file
-//   --stdout-to-log                redirect std console output from console to
-//                                  log file
-//   --public arg                   indicate if the server should allow other
-//                                  players to join
-//   --listed arg                   indicate if the server should show up on
-//                                  public server lists
-//   --authentication arg           enables authentication of players
-//   --use-steam-networking arg     use steam networking and authentication (if
-//                                  enabled) for users
-//   --immediate-writeout arg       immediately write player data to disk when it
-//                                  changes. decreases performance during sector
-//                                  changes, but makes server data more consistent
-//                                  on crash.
-//   --max-logs arg                 maximum number of logs to keep around, 0 for
-//                                  infinite, default: 15
-//   --rcon-ip arg                  binds the rcon server to a specific IP
-//   --rcon-port arg                rcon port, default: 27015
-//   --rcon-password arg            sets the password for the rcon interface.
-//                                  without password, rcon is disabled.
-//   --send-crash-reports arg       when enabled, the server will send anonymous
-//                                  system specs and a crash report when it
-//                                  crashes.
 
 const windows = {
   exec: path.resolve(globals.InstallationDir()+'/dsm/avorion/bin/AvorionServer.exe')
@@ -86,7 +15,6 @@ const linux = {
 }
 
 const startGameServer = (GameServerEmitter, startupParams, supressLogs = false) => {
-  // Console(stdout[, stderr][, ignoreErrors])
 
   // ????
   // execvp(3) failed.: Permission denied
@@ -169,7 +97,6 @@ const startGameServer = (GameServerEmitter, startupParams, supressLogs = false) 
 
     // Server startup FAILED
   });
-
 
   GameServer.on('error',(error)=>{
     if(!supressLogs)
